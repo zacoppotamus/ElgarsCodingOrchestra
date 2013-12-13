887b129887b9cdc322f8b7cd27f2cbecfe052c7d
Luke Zbihlyj <me@lukezbihlyj.co.uk>
2013-12-13 04:07:36 +0000

    Added field parameter.

--- api/query.php
+++ api/query.php
@@ -11,7 +11,8 @@ include("includes/api/core.php");
 $data = array(
     "dataset" => (isset($_GET['d'])) ? trim(strtolower($_GET['d'])) : null,
     "offset" => (isset($_GET['offset']) && intval($_GET['offset']) >= 0) ? intval($_GET['offset']) : 0,
-    "rows" => (isset($_GET['rows']) && intval($_GET['rows']) >= 1) ? intval($_GET['rows']) : -1
+    "rows" => (isset($_GET['rows']) && intval($_GET['rows']) >= 1) ? intval($_GET['rows']) : -1,
+    "fields" => (isset($_GET['fields'])) ? json_decode($_GET['fields'], true) : null
 );
 
 /*!
@@ -40,19 +41,36 @@ $collection = mongocli::select_collection($data['dataset']);
 
 /*!
  * Perform our 'find' query based on the information fed to the
- * API.
+ * API. We have to check if some parameters are set/valid here.
  */
 
-$query = $collection->find();
+$query = null;
+$fields = null;
 
+// Check if any field names were sent.
+if(isset($data['fields']) && !empty($data['fields'])) {
+    if(is_array($data['fields'])) {
+        $fields = $data['fields'];
+    } else {
+        echo json_beautify(json_render_error(402, "You didn't specify the field names correctly, they should be in the form: ['field1', 'field2']."));
+        exit;
+    }
+}
+
+// Run the query.
+$query = $collection->find($query, $fields);
+
+// Set the offset if we have one.
 if($data['offset'] > 0) {
     $query = $query->skip($data['offset']);
 }
 
+// If we have a row limit, apply it.
 if($data['rows'] > -1) {
     $query = $query->limit($data['rows']);
 }
 
+// Iterate through the results and populate the output.
 foreach($query as $row) {
     $json['rows']++;
     $json['results'][] = $row;
