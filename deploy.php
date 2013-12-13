28ea93a48bdd86091b0452e9c7f8de0bf002b691
Luke Zbihlyj <me@lukezbihlyj.co.uk>
2013-12-13 02:06:37 +0000

    Debugging deploy script.

--- deploy.php
+++ deploy.php
@@ -5,11 +5,11 @@ header("content-type: text/plain; charset=utf8");
 
 // Define the commands to be run.
 $commands = array(
-    "git pull",
-    "git status",
-    "git submodule sync",
-    "git submodule update",
-    "git submodule status"
+    "/usr/bin/git pull 2&>1",
+    "/usr/bin/git status 2&>1",
+    "/usr/bin/git submodule sync 2&>1",
+    "/usr/bin/git submodule update 2&>1",
+    "/usr/bin/git submodule status 2&>1"
 );
 
 // Run them all.
