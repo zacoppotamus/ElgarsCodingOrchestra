4f683ee2d7a05da0ca3031f0fc8d9dc414db14b7
Ben Elgar <me@benelgar.com>
2013-12-15 13:15:36 +0000

    Added passing in arguments

--- correlation/correlation.py
+++ correlation/correlation.py
@@ -1,5 +1,10 @@
-import numpy, json
+import numpy      # To calculate correlation
+import json       # To convert to JSON
+import sys        # To get arguments
+import warnings   # To suppress numpy warnings
+
 from pymongo import MongoClient
+warnings.simplefilter('ignore', numpy.RankWarning)
 
 def graphEquation(x1Points, x2Points, yPoints, degree, x1Weight=1, x2Weight=1):
     if(x1Weight > 1 or x1Weight < 0 or x2Weight > 1 or x2Weight < 0):
@@ -19,12 +24,6 @@ def query(databaseName, collectionName, x1Name, x2Name, yName, degree, x1Weight=
     db         = client[databaseName]
     collection = db[collectionName]
 
-    #post = {"crime": 10,
-            #"budget": 1000,
-            #"year": 2000}
-
-    #collection.insert(post)
-
     x1Points = [p[x1Name] for p in collection.find()]
     x2Points = [p[x2Name] for p in collection.find()]
     yPoints  = [p[yName ] for p in collection.find()]
@@ -34,7 +33,8 @@ def query(databaseName, collectionName, x1Name, x2Name, yName, degree, x1Weight=
     return json.dumps(equation)
 
 def main():
-    query("eco", "crime", "budget", "year", "crime", 2);
+    #print sys.argv
+    query(sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4], sys.argv[5], sys.argv[6])
 
 
 if __name__ == "__main__":
