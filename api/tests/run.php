e1a8805e733560b900293a14fe79ff6520600e92
Luke Zbihlyj <me@lukezbihlyj.co.uk>
2013-12-15 19:45:41 +0000

    Added /delete command and working on test cases.

---
+++ api/tests/run.php
@@ -0,0 +1,30 @@
+<?php
+
+include("includes/kernel.php");
+include("includes/api/core.php");
+
+/*!
+ * Test one: Inserting data.
+ */
+
+// Insert data.
+
+/*!
+ * Test two: Select data.
+ */
+
+// Select some random data.
+
+/*!
+ * Test three: Update data.
+ */
+
+// Update some data.
+
+/*!
+ * Test four: Delete data.
+ */
+
+// Delete all the data.
+
+?>
