fd5660d6f05f15b5035cc4f164cbb408d84caebe
Luke Zbihlyj <me@lukezbihlyj.co.uk>
2013-12-18 23:24:34 +0000

    Testing data import.

---
+++ api/tests/import.php
@@ -0,0 +1,51 @@
+<?php
+
+include("includes/kernel.php");
+include("includes/api/core.php");
+include("includes/classes/eco.class.php");
+
+/*!
+ * Define a function to turn a CSV into an array.
+ */
+
+function csv_to_array($filename = "", $delimiter = ",") {
+    if(!file_exists($filename) || !is_readable($filename)) {
+        return false;
+    }
+
+    $header = null;
+    $data = array();
+
+    if(($handle = fopen($filename, 'r')) !== false) {
+        while(($row = fgetcsv($handle, 1000, $delimiter)) !== false) {
+            if(!$header) {
+                $header = $row;
+            } else {
+                $data[] = array_combine($header, $row);
+            }
+        }
+
+        fclose($handle);
+    }
+
+    return $data;
+}
+
+// Define our dataset name.
+$dataset = "nysubway";
+
+// Get our data.
+$data = csv_to_array("../frontend/visualizations/nysubway/subentrances.csv");
+
+// Import data.
+if(!empty($data)) {
+    $insert = eco::insert_multi($dataset, $data);
+
+    if($insert) {
+        echo "[+] Added!";
+    } else {
+        echo "[!] Failed! " . eco::last_error();
+    }
+}
+
+?>
