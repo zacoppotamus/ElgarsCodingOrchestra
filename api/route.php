fe8d13287dd26407f97d3a77fb5bec615176ec72
Luke Zbihlyj <me@lukezbihlyj.co.uk>
2013-12-24 23:31:41 +0000

    Adding CORS policy header to all requests.

--- api/route.php
+++ api/route.php
@@ -7,6 +7,7 @@ ini_set("display_errors", 1);
 // Set the header content type including charset.
 include("includes/classes/route.class.php");
 header("content-type: application/json; charset=utf8");
+header("access-control-allow-origin: *");
 
 /*!
  * Add all of the possible routes to the class, including
