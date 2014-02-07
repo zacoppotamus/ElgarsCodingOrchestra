eee6756389a87c9c25337769dd1d2c8b15860b76
Luke Zbihlyj <me@lukezbihlyj.co.uk>
2014-02-07 00:09:11 +0000

    Missed a file.

---
+++ api/datasets.php
@@ -0,0 +1,38 @@
+<?php
+
+include("includes/kernel.php");
+include("includes/api/core.php");
+
+/*!
+ * Grab all of the different inputs so that we can use them elsewhere
+ * in this script.
+ */
+
+$data = array();
+
+/*!
+ * Define an empty array to store the results of whatever we need
+ * to send back.
+ */
+
+$json = array(
+    "datasets" => array()
+);
+
+// Run the query to find the datasets.
+try {
+    $json['datasets'] = mongocli::get_collections();
+} catch(Exception $e) {
+    echo json_beautify(json_render_error(404, "An unexpected error occured while trying to find the datasets."));
+    exit;
+}
+
+/*!
+ * Output our JSON payload for use in whatever needs to be using
+ * it.
+ */
+
+echo json_beautify(json_render(200, $json));
+exit;
+
+?>
