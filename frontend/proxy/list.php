1197a44aba61590e5cfef49a2f4d0e086dec4b95
Luke Zbihlyj <me@lukezbihlyj.co.uk>
2014-02-11 16:51:30 +0000

    Added error checking.

--- frontend/proxy/list.php
+++ frontend/proxy/list.php
@@ -1,17 +1,26 @@
 <?php
 
 include("eco.class.php");
+header("content-type: application/json; charset=utf8");
 
 $eco = new eco("eSQpirMYxjXUs8xIjjaUo72gutwDJ4CP");
 
 $dataset = isset($_GET['dataset']) ? $_GET['dataset'] : null;
 $query = isset($_GET['query']) ? json_decode($_GET['query']) : null;
-$offset = isset($_GET['offset']) ? (int)$_GET['offset'] : 0;
 $limit = isset($_GET['limit']) ? (int)$_GET['limit'] : null;
+$offset = isset($_GET['offset']) ? (int)$_GET['offset'] : 0;
 $fields = isset($_GET['fields']) ? json_decode($_GET['fields']) : null;
 
 $records = array();
-$results = $eco->select($dataset, $query, $offset, $limit, $fields);
+$results = $eco->select($dataset, $query, $limit, $offset, $fields);
+
+if(!$results) {
+    echo json_encode(array(
+        "Result" => "Fail",
+        "Message" => $eco->error()
+    ));
+    exit;
+}
 
 foreach($results['rows'] as $row) {
     $records[] = $row;
