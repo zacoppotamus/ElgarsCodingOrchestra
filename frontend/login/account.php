41f68d4d4b78cd1d102dbb2586564884ca792da9
Ben Elgar <me@benelgar.com>
2014-02-27 22:19:54 +0000

    Tried to catch failed login

--- frontend/login/account.php
+++ frontend/login/account.php
@@ -12,6 +12,11 @@ curl_setopt($ch, CURLOPT_HTTPHEADER, array("X-Mashape-Authorization: " . $mashap
 curl_setopt($ch, CURLOPT_TIMEOUT, 10);
 
 $result = curl_exec($ch);
+if ($result == "{\"message\":\"Invalid Mashape key. If you are a Mashape user, get your key from your dashboard at https://www.mashape.com/login - To create a free Mashape account instead, go to https://www.mashape.com/signup\"}")
+{
+    $result = "Failed login";
+}
+
 curl_close($ch);
 ?>
 
@@ -20,6 +25,7 @@ curl_close($ch);
         <title>Account Page</title>
     </head>
     <body>
+        <div id="Result"></div>
         <p>Your API key is <?php echo $mashape_key; ?></p>
         <p>Your result is <?php echo $result; ?></p>
     </body>
