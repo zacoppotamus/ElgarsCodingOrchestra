8ae7cd4e14a834492f3c2990aad4b385de3f305e
Ben Elgar <me@benelgar.com>
2014-02-27 20:45:53 +0000

    Attempted to show datasets for specific api key

---
+++ frontend/account.php
@@ -0,0 +1,25 @@
+<?php
+private $mashape_key = $_POST["apiKey"];
+
+$ch = curl_init();
+
+$url = "https://sneeza-eco.p.mashape.com/datasets"
+
+curl_setopt($ch, CURLOPT_URL, $url);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_USERAGENT, "ECO / Login System 0.1");
+curl_setopt($ch, CURLOPT_HTTPHEADER, array("X-Mashape-Authorization: " . $this->mashape_key));
+
+$result = curl_exec($ch);
+curl_close($ch);
+?>
+
+<html>
+    <head>
+        <title>Account Page</title>
+    </head>
+    <body>
+        <p>Your API key is <?php echo $mashape_key; ?></p>
+        <p><?php echo $result ?></p>
+    </body>
+</html>
