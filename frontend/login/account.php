e688fe16868445e411ddf5e772a6b31d17a4a19a
Ben Elgar <me@benelgar.com>
2014-02-27 21:47:39 +0000

    Removed 'this' keyword

--- frontend/login/account.php
+++ frontend/login/account.php
@@ -8,7 +8,7 @@ $url = "https://sneeza-eco.p.mashape.com/datasets";
 curl_setopt($ch, CURLOPT_URL, $url);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 curl_setopt($ch, CURLOPT_USERAGENT, "ECO / Login System 0.1");
-curl_setopt($ch, CURLOPT_HTTPHEADER, array("X-Mashape-Authorization: " . $this->mashape_key));
+curl_setopt($ch, CURLOPT_HTTPHEADER, array("X-Mashape-Authorization: " . $mashape_key));
 curl_setopt($ch, CURLOPT_TIMEOUT, 10);
 
 $result = curl_exec($ch);
