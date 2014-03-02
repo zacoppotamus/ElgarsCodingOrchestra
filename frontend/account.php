ca61803f5e7a076692eacf58e6f8d753353d2988
BenElgar <me@benelgar.com>
2014-03-02 22:06:11 +0000

    Returned cookie functionality

--- frontend/account.php
+++ frontend/account.php
@@ -1,8 +1,8 @@
 <?php
 
-//$mashape_key = isset($_POST["apiKey"]) ? $_POST["apiKey"] : $_COOKIE["apiKey"];
+$mashape_key = isset($_POST["apiKey"]) ? $_POST["apiKey"] : $_COOKIE["apiKey"];
 //$mashape_key = isset($_POST["apiKey"]) ? $_POST["apiKey"] : null;
-$mashape_key = $_POST["apiKey"];
+//$mashape_key = $_POST["apiKey"];
 
 function getRequest($requestURL, $auth_key)
 {
@@ -25,10 +25,10 @@ if (stristr($datasetsInfo["message"], "Invalid Mashape key"))
 {
     header('Location: login.html');
 }
-//else
-//{
-    //setcookie(apiKey, $mashape_key, 0, "/", "project.spe.sneeza.me", isset($_SERVER["HTTPS"]), true);
-//}
+else
+{
+    setcookie(apiKey, $mashape_key, 0, "/", "project.spe.sneeza.me", isset($_SERVER["HTTPS"]), true);
+}
 ?>
 
 <!DOCTYPE html>
