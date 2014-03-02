80023d1618ed033d44a82fd2e9007e0f39bfe39c
Ben Elgar <me@benelgar.com>
2014-03-02 15:12:21 +0000

    Changed for testing

--- frontend/edit.php
+++ frontend/edit.php
@@ -53,7 +53,7 @@ $fields = $result["data"]["fields"];
                                 {
                                     if($fields[$i] !== "_id")
                                     {
-                                        echo ("$fields[$i],{title:''},");
+                                        echo ("$fields[$i]: {title:'$fields[$i]'},");
                                         //echo $fields[$i]."{title: '" . $fields[$i] . "'},";
                                     }
                                 }
