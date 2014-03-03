a842d1fd6013888ce61903276645022ef44b1e06
BenElgar <me@benelgar.com>
2014-03-03 00:50:25 +0000

    Added a surrounding bootstrap row

--- frontend/login.php
+++ frontend/login.php
@@ -16,25 +16,27 @@ if(isset($_COOKIE["apiKey"]))
     </head>
     <body>
         <div class="container">
-            <p>Please insert your API key</p>
-            <form action="account.php" role = form method="post">
-                <div class="form-group">
-                    <label for="apiKey">API Key</label>
-                    <input type="text" placeholder="API Key" name="apiKey" autofocus>
-                </div>
-                <button type="submit" class="btn btn-default">Submit</button>
-            </form>
-            <?php
-            if(isset($_GET["fail"]))
-            {
-               echo "<p>Incorrect API Key.</p>";
-            }
-            elseif(isset($_GET["logout"]))
-            {
-                setcookie(apiKey, "", time()-3600, "/", "project.spe.sneeza.me", isset($_SERVER["HTTPS"]), false);
-                echo "<p>Successfully logged out.</p>";
-            }
-            ?>
+            <div class="row">
+                <p>Please insert your API key</p>
+                <form action="account.php" role = form method="post">
+                    <div class="form-group">
+                        <label for="apiKey">API Key</label>
+                        <input type="text" placeholder="API Key" name="apiKey" autofocus>
+                    </div>
+                    <button type="submit" class="btn btn-default">Submit</button>
+                </form>
+                <?php
+                if(isset($_GET["fail"]))
+                {
+                   echo "<p>Incorrect API Key.</p>";
+                }
+                elseif(isset($_GET["logout"]))
+                {
+                    setcookie(apiKey, "", time()-3600, "/", "project.spe.sneeza.me", isset($_SERVER["HTTPS"]), false);
+                    echo "<p>Successfully logged out.</p>";
+                }
+                ?>
+            </div>
         </div>
     </body>
 </html>
