ed36c2f73cb93cd78769e1c9cef54e2100442b4c
Zac Ioannidis <zacoppotamus@gmail.com>
2014-03-03 01:46:27 +0000

    Something something barchart

--- frontend/newlogic/js/directives.js
+++ frontend/newlogic/js/directives.js
@@ -7,9 +7,9 @@ angular.module('eco.directives', [])
 .directive('d3Visualization', function() {
 
 	// constants
-	var margin = 20,
-		width  = 960,
-		height = 500 - margin;
+	var margin = {top:20, right:20, bottom:30, left:40},
+		width  = 1100 - margin.left - margin.right,
+		height = 500 - margin.top - margin.bottom;
 
 	return {
 		restrict: 'E',
@@ -20,8 +20,10 @@ angular.module('eco.directives', [])
 		link: function (scope, element, attrs) {
 			var svg = d3.select(element[0])
 				.append("svg")
-					.attr("width", width)
-					.attr("height", height + margin + 100);
+					.attr("width", width + margin.left + margin.right)
+					.attr("height", height + margin.top + margin.bottom)
+				.append("g")
+					.attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
 			scope.$watch('year', function (newVal, oldVal) {
 
@@ -34,34 +36,80 @@ angular.module('eco.directives', [])
 				}
 
 				d3.json('/samples/nba_payrolls.json', function(error, json){
+					// number of teams
+					var teamNo = 30;
 					var currentYear = newVal;
 					var dataset = json[currentYear];
-					console.log(dataset);
-					var colorScale = d3.scale.category20();
-					var yScale = d3.scale.linear()
-						.domain([0, d3.max(dataset)])
-						.range([0, height]);
+					var barWidth = width/teamNo;
+
+					// get max team payroll for this year
+					var maxPayroll = d3.max(d3.values(json[currentYear]), function(teamnode) {
+						return teamnode.payroll;
+					});
 
 					var xScale = d3.scale.ordinal()
-						.domain(d3.range(dataset.length))
-						.rangeRoundBands([0, width], 0.05);
+						.domain(d3.range(teamNo))
+						.rangeRoundBands([0, width], .1);
 
-					console.log(newVal);
+					var yScale = d3.scale.linear()
+						.domain([0, maxPayroll])
+						.range([height, 0]);
 
-					console.log('bla');
-					svg.selectAll("rect")
-						.data(dataset)
+					var colorScale = d3.scale.category20();
+
+					var bar = svg.selectAll("g")
+						.data(d3.values(dataset), function(d) {
+							return d.payroll;
+						})
 						.enter()
-						.append('rect')
+						.append("g");
+
+					var yAxis = d3.svg.axis()
+						.scale(yScale)
+						.orient("left")
+						.ticks(10);
+
+					svg.append("g")
+							.attr("class", "y axis")
+							.call(yAxis)
+						.append("text")
+							.attr("transform", "rotate(-90)")
+							.attr("y", 6)
+							.attr("dy", ".21em")
+							.style("text-anchor", "end")
+							.style("font", "10px Helvetica")
+							.text("Payroll");
+
+					bar.append("rect")
 						.attr('x', function(d,i) {
 							return xScale(i);
 						})
 						.attr('y', function(d) {
 							return yScale(d.payroll);
 						})
+						.attr('height', function(d) {
+							return height - yScale(d.payroll);
+						})
+						.attr('width', xScale.rangeBand())
 						.attr("fill", function(d,i) {
 							return colorScale(i);
 						});
+
+
+					// add labels
+					bar.append("text")
+						.attr("text-anchor", "middle")
+						.attr("x", function(d, i) {
+							return xScale(i) + xScale.rangeBand() / 2;
+						})
+						.attr("y", function(d) {
+							return yScale(d.payroll) - 10;
+						})
+						.attr("dy", ".35em")
+						.attr("height", 10)
+						.attr("width", 20)
+						.text(function(d) { return d['team']; });
+
 				});
 
 			});
