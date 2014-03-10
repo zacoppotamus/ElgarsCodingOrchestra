ea1a2c9a6369d5d13739c9cb54c7517bcd61bcc5
BenElgar <me@benelgar.com>
2014-03-10 01:33:08 +0000

    Changed size of graph

--- frontend/piechart.php
+++ frontend/piechart.php
@@ -38,7 +38,9 @@ $fields = $rainhawk->fetchDataset($dataset)["fields"];
 
     function drawChart() {
       var jsonData = $.ajax({
-          url: "http://project.spe.sneeza.me/datatable.php?dataset=benelgar.test&fields=[%22"+ document.getElementById("xName").value +"%22,%22" + document.getElementById("yName").value + "%22]",
+          url: "http://project.spe.sneeza.me/datatable.php?dataset=benelgar.test&fields=[%22" +
+            document.getElementById("xName").value +"%22,%22" +
+            document.getElementById("yName").value + "%22]",
           dataType:"json",
           async: false
           }).responseText;
@@ -93,7 +95,7 @@ $fields = $rainhawk->fetchDataset($dataset)["fields"];
           </div>
       </div>
       <div class="row">
-        <div id="chart_div" style="height: 50%"></div>
+        <div id="chart_div" style="height: 80%"></div>
       </div>
   </body>
 </html>
