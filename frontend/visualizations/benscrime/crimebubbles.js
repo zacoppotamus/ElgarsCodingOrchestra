4747feb0e3cb00457f5ca0bd45771e1f053155c5
Oscar Scull <oscscull@gmail.com>
2014-03-12 23:13:41 +0000

    made crimebubbles into a force directed graph. you can drag around the bubbles and they will resettle. next step is to be able to switch datasets; and have the bubbles charge (and all sizes) done on the fly with angular.

--- frontend/visualizations/benscrime/crimebubbles.js
+++ frontend/visualizations/benscrime/crimebubbles.js
@@ -1,4 +1,4 @@
-var jsonYB = [
+var nodes = [
   {
     "Year":1997,
     "Budget":1400
@@ -72,27 +72,36 @@ var jsonYB = [
     "Budget":7362
   }
 ]
-var svgContainer = d3.select("#Something")
-        .append("svg")
-        .attr("width", 2000)
-        .attr("height", 2000);    
-var nodes = svgContainer.selectAll("g")
-    .data(jsonYB)
-    .enter()
-    .append("g")
-    .attr("transform", function(d){return "translate(" + (d.Year - 1996) * 60 + ","+ (d.Year - 1996) * 100 +")"})
-var circles = nodes.append("circle")
-var text = nodes.append("text")
+var width = 1000,
+    height = 1000;
+    
+var force = d3.layout.force()
+    .nodes(nodes)
+    .size([width,height])
+    .on("tick",tick)
+    .charge(-1000)
+    .start();
+    
+var svg = d3.select("#container").append("svg")
+    .attr("width",width)
+    .attr("height",height)
+
+var node = svg.selectAll("g")
+    .data(force.nodes())
+    .enter().append("g")
+    .call(force.drag)
+    
+var circle = node.append("circle")
+    .style("fill","aqua")
+    .style("stroke", "gray")
+    .attr("r", function(d){return d.Budget/100})
+
+var text = node.append("text")
     .text(function(d) {return d.Year;})
     .attr("text-anchor","middle")
-var circleAttributes = circles
-    .attr("r", function(d) {return (d.Budget/100);})
-   // .attr("cx", function(d){return (d.Year - 1995) * 50;})
-   // .attr("cy", function(d){return (d.Year - 1995) * 50;})
-    .style("stroke", "gray")
-    .style("fill", "aquamarine")
-    .on("mouseover", function(){d3.select(this).style("fill", "aqua");})
-    .on("mouseout", function(){d3.select(this).style("fill", "aquamarine");});
-
-
-
+    
+function tick(){
+    node.attr("transform", function(d){return "translate(" + d.x + ","+ d.y +")"})
+    circle.on("mouseover", function(){d3.select(this).style("fill", "aquamarine");})
+    circle.on("mouseout", function(){d3.select(this).style("fill", "aqua");});
+}
