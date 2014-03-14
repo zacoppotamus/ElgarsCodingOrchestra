5e9f267b51c78b922387e9ae3a4c97e234eb7ee6
Luke Zbihlyj <me@lukezbihlyj.co.uk>
2014-03-14 02:29:05 +0000

    Oops.

--- deploy.php
+++ deploy.php
@@ -22,7 +22,7 @@ $commands = array(
 foreach($commands as $command) {
     echo "$ {$command}\n";
 
-    $output = shell_exec("export PATH='/usr/local/bin/'; " . $command . " 2>&1");
+    $output = shell_exec("export PATH=/usr/local/bin:/usr/bin:/bin; " . $command . " 2>&1");
     echo "{$output}\n";
 }
 
