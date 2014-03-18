dd237da2e7136d90bd042890437dde31da1d1d33
BenElgar <me@benelgar.com>
2014-03-18 18:51:09 +0000

    Changed language for edit

--- frontend/edit.php
+++ frontend/edit.php
@@ -13,7 +13,7 @@ $fields      = $datasetInfo["fields"];
 
 ?>
 <!doctype html>
-<html lang="en">
+<html lang="en-gb">
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
