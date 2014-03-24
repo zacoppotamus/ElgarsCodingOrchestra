202b62e3739ca2e23128a233e451a112435e83a9
Luke Zbihlyj <me@lukezbihlyj.co.uk>
2014-03-24 21:53:36 +0000

    Started work on Javascript wrapper.

---
+++ wrappers/javascript/rainhawk.js
@@ -0,0 +1,14 @@
+/**
+ * Project Rainhawk
+ *
+ * Simple PHP wrapper for the Rainhawk API, which provides simple JSON
+ * encoded data for a variety of data sources with pre-defined
+ * operations.
+ *
+ * @package Rainhawk
+ * @license none
+ */
+
+var rainhawk = {
+
+};
